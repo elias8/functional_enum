4deea6439608a3aa7ba54199dd1a9450502870bf
Elias Andualem <eliasandualem8@gmail.com>
2021-06-24 19:20:24 +0300

    changed annotation name to functionalEnum

--- packages/functional_enum_annotation/lib/functional_enum_annotation.dart
+++ packages/functional_enum_annotation/lib/functional_enum_annotation.dart
@@ -2,7 +2,7 @@ library functional_enum_annotation;
 
 export 'package:meta/meta.dart';
 
-const functional_enum = FunctionalEnum._();
+const functionalEnum = FunctionalEnum._();
 
 class FunctionalEnum {
   const FunctionalEnum._();
