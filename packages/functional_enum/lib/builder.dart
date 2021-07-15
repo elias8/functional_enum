2098d1fddd1b9608a25d8bed8ada713415e710be
Elias Andualem <eliasandualem8@gmail.com>
2021-07-15 09:09:27 +0300

    - use shared builder to write generated code to ".g.dart" file - :arrow_up: upgraded functional_annotation dependency - removed meta dependency

--- packages/functional_enum/lib/builder.dart
+++ packages/functional_enum/lib/builder.dart
@@ -5,5 +5,5 @@ import 'package:functional_enum/src/functional_enum_generator.dart';
 import 'package:source_gen/source_gen.dart';
 
 Builder functionalEnum(BuilderOptions options) {
-  return PartBuilder([FunctionalEnumGenerator()], '.g.dart');
+  return SharedPartBuilder([FunctionalEnumGenerator()], 'enum');
 }
