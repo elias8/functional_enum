c787b778a22967624dafcbad9a785cef8e7300ea
Elias Andualem <eliasandualem8@gmail.com>
2021-09-23 21:22:52 +0300

    :arrow_up: upgraded dependencies :memo: added some doc

--- packages/functional_enum/lib/builder.dart
+++ packages/functional_enum/lib/builder.dart
@@ -4,6 +4,12 @@ import 'package:build/build.dart';
 import 'package:functional_enum/src/functional_enum_generator.dart';
 import 'package:source_gen/source_gen.dart';
 
+/// Configuration for using `package:build`-compatible build systems.
+///
+/// See:
+/// * [build_runner](https://pub.dev/packages/build_runner)
+///
+/// This library is **not** intended to be imported by typical end-users.
 Builder functionalEnum(BuilderOptions options) {
-  return SharedPartBuilder([FunctionalEnumGenerator()], 'enum');
+  return SharedPartBuilder([const FunctionalEnumGenerator()], 'enum');
 }
